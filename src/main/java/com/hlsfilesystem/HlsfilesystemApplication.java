7e269a588847f631cba168fb9ba04629261f8de0
hucheng <1278993876@qq.com>
2019-08-09 17:06:15 +0800

    hlsfilesystem配置开启事务支持

--- src/main/java/com/hlsfilesystem/HlsfilesystemApplication.java
+++ src/main/java/com/hlsfilesystem/HlsfilesystemApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@MapperScan("com.hlsfilesystem.mapper")
-@EnableScheduling
+@MapperScan("com.hlsfilesystem.mapper")//扫描mapper包
+@EnableScheduling//开启定时支持
+@EnableTransactionManagement//开启事务支持
 public class HlsfilesystemApplication {
 
 	public static void main(String[] args) {
